4d84a08fd8888f4adf19d454bb49d2a54b505f14
Rexmicool <matthieu.barraud@gmail.com>
2025-02-05 12:01:35 +0100

    Add entrypoint script and configure Dockerfile; update settings for static files

---
+++ ewallet/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Stop on errors
+set -e
+
+echo "Applying database migrations..."
+python manage.py migrate --noinput
+
+echo "Collecting static files..."
+python manage.py collectstatic --noinput
+
+echo "Starting application..."
+exec "$@"
--- ewallet/ewallet/urls.py
+++ ewallet/ewallet/urls.py
@@ -20,3 +20,12 @@ from django.urls import path
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+
+
+
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
