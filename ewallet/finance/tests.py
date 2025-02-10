dbad22cebaddbe51a20a68c97c7f09274ddb6255
Rexmicool <matthieu.barraud@gmail.com>
2025-02-10 15:36:43 +0100

    refactor tests to use get_user_model and update stock creation with current price

--- ewallet/finance/tests.py
+++ ewallet/finance/tests.py
@@ -1,12 +1,14 @@
 from django.test import TestCase
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 from .models import Portfolio, Stock, PortfolioStock
+
+User = get_user_model()
 
 class PortfolioTestCase(TestCase):
     def setUp(self):
         self.user = User.objects.create_user(username="testuser", password="testpass")
-        self.portfolio = Portfolio.objects.create(user=self.user, name="Mon Portefeuille")
-        self.stock = Stock.objects.create(symbol="AAPL", name="Apple Inc.")
+        self.portfolio = Portfolio.objects.create(name="Mon Portefeuille", user=self.user)
+        self.stock = Stock.objects.create(symbol="AAPL", current_price=0.00)
         self.portfolio_stock = PortfolioStock.objects.create(
             portfolio=self.portfolio,
             stock=self.stock,
