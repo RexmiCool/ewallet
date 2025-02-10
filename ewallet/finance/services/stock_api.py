cc43178fa0314e4aa10b4c32058a421c3f10a173
Rexmicool <matthieu.barraud@gmail.com>
2025-02-10 15:22:55 +0100

    add API key configuration and enhance portfolio navigation

--- ewallet/finance/services/stock_api.py
+++ ewallet/finance/services/stock_api.py
@@ -1,37 +1,8 @@
 import requests
+from decouple import config
 
-API_KEY = "KP5HP5ANEKBE72QE"  # Remplace par ta clé API
-#API_KEY = "1X5QGTSJ8FZORWN4"  # Remplace par ta clé API
-#API_KEY = "XPB81I0K815170J2"  # Remplace par ta clé API
-#API_KEY = "FUU41PKIXK75ZXJ2"
-API_URL = "https://www.alphavantage.co/query"
-
-def get_stock_price_old(symbol):
-    params = {
-        "function": "GLOBAL_QUOTE",
-        "symbol": symbol,
-        "apikey": API_KEY
-    }
-    print("get_stock_price")
-    print("symbol")
-    print(symbol)
-    response = requests.get(API_URL, params=params)
-    data = response.json()
-    
-    print("data")
-    print(data)
-    
-    try:
-        return float(data["Global Quote"]["05. price"])  # Prix actuel
-    except KeyError:
-        return None  # En cas d'erreur
-
-
-
-
-import requests
-
-API_KEY = "P5PuKDmXNiltclmPsSTnrph3X8anpjNv"
+#API_KEY = "P5PuKDmXNiltclmPsSTnrph3X8anpjNv"
+API_KEY = config('API_KEY')
 API_URL = "https://financialmodelingprep.com/api/v3/quote"
 
 def get_stock_price(symbol):
@@ -41,9 +12,6 @@ def get_stock_price(symbol):
     url = f"{API_URL}/{symbol}"
     response = requests.get(url, params=params)
     data = response.json()
-    
-    print("data")
-    print(data)
     
     try:
         return float(data[0]["price"])  # Prix actuel
